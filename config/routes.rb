8b083105090550655497a82d91af04dbd3df4ad0
Katie <katie.vandervoort@gmail.com>
2020-05-06 16:55:39 -0700

    created Passengers controller, index method, index view, index route, generated the Passenger  model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/drivers', to: 'drivers#index', as: 'drivers'
+
+
+  get '/passengers', to: 'passengers#index', as: 'passengers'
 end
