c517fb21eda2edb2b208b67a46049d4b48263b1d
Leah Hughes <leahwhughes@gmail.com>
2020-05-07 15:20:24 -0700

    adds driver show route, method, skeleton view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,7 +1,22 @@
 class DriversController < ApplicationController
-
+  
   def index
-    # @drivers = Driver.all # maybe add: .order(:id)
+    @drivers = Driver.all # maybe add: .order(:id)
+  end
+  
+  def show
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:id, :name, :vin, :available)
   end
 
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   get '/drivers', to: 'drivers#index', as: 'drivers'
+  # post '/drivers', to: 'drivers#create'
+  get '/drivers/:id', to: 'drivers#show', as: 'new_driver'
 
+  # get    "/books"          , to: "books#index",   as: :book
+  # post   "/books"          , to: "books#create"
+  # get    "/books/new"      , to: "books#new",     as: :new_book
+  # get    "/books/:id"      , to: "books#show",    as: :book
+  # patch  "/books/:id"      , to: "books#update"
+  # put    "/books/:id"      , to: "books#update"
+  # delete "/books/:id"      , to: "books#destroy"
+  # get    "/books/:id/edit" , to: "books#edit",    as: :edit_book
 
+  
   get '/passengers', to: 'passengers#index', as: 'passengers'
-
-
+  
+  
   get '/trips', to: 'trips#index', as: 'trips'
 end
