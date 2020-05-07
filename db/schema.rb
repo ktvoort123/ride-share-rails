42b470d7e94157b72fb85ea79b57fdc906703960
Leah Hughes <leahwhughes@gmail.com>
2020-05-07 14:01:51 -0700

    adds relate trips to drivers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_002209) do
+ActiveRecord::Schema.define(version: 2020_05_07_202047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,9 @@ ActiveRecord::Schema.define(version: 2020_05_07_002209) do
     t.integer "cost"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "driver_id"
     t.integer "passenger_id"
+    t.bigint "driver_id"
+    t.index ["driver_id"], name: "index_trips_on_driver_id"
   end
 
 end
