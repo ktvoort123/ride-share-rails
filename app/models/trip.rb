cfedf2e5d369359f206946f0d33d14b198ac4b3e
Katie <katie.vandervoort@gmail.com>
2020-05-08 13:45:37 -0700

    added validation to driver, passenger, and trip, and added error message display to driver, trip, passenger views

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+  validates :date, presence: true
+  validates :cost, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6, allow_nil: true }
 end
