3a761f79d2a8d696490c5ac6499296893b158cf0
Katie <katie.vandervoort@gmail.com>
2020-05-10 10:19:36 -0700

    added total_spent method to passenger model and called method in passenger show view

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,10 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_spent
+    return self.trips.map { |trip| trip.cost }.sum
+  end
+
+  
 end
